e958f3e7f5330749a758d3d80e31374617de7be5
unknown <naiknihal57@gmail.com>
2026-07-01 14:13:43 +0530

    final-pipeline

--- application.groovy
+++ application.groovy
@@ -1,6 +1,75 @@
-pipeline {
-   agent any
-   stages { 
-     stage ('PULL STAGE') {
-       steps {
-         git '
+pipeline {i
+    agent any
+
+    stages {
+
+        stage('PULL STAGE') {
+            steps {
+                git 'https://github.com/nnihal7/final-jenkins-app-repo.git'
+            }
+        }
+
+        stage('FRONTEND-DOCKER-BUILD') {
+            steps {
+                sh '''
+                cd frontend
+                docker build -t nihalnaik7/easy-frontend:latest .
+                '''
+            }
+        }
+
+        stage('BACKEND-DOCKER-BUILD') {
+            steps {
+                sh '''
+                cd backend
+                docker build -t nihalnaik7/easy-backend:latest .
+                '''
+            }
+        }
+
+        stage('DOCKER-PUSH') {
+            steps {
+                withCredentials([
+                    usernamePassword(
+                        credentialsId: 'dockerhub',
+                        usernameVariable: 'DOCKER_USER',
+                        passwordVariable: 'DOCKER_PASS'
+                    )
+                ]) {
+                    sh '''
+                    echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+
+                    docker push nihalnaik7/easy-frontend:latest
+                    docker push nihalnaik7/easy-backend:latest
+
+                    docker logout
+                    '''
+                }
+            }
+        }
+
+        stage('DOCKER-CLEAN') {
+            steps {
+                sh '''
+                docker rmi -f nihalnaik7/easy-frontend:latest || true
+                docker rmi -f nihalnaik7/easy-backend:latest || true
+                '''
+            }
+        }
+
+        stage('DEPLOY') {
+           steps {
+               sh '''
+        aws eks update-kubeconfig \
+          --region ap-south-1 \
+          --name my-cluster
+
+        kubectl get nodes
+
+        kubectl apply -f simple-deploy/ '''
+           }
+        }
+    }
+    
+}
+
